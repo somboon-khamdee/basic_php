4aa0b5bc16196780665a928a53adaeae5ae2e0aa
somboon-khamdee <somboon.khumdee@gmail.com>
2021-12-21 11:19:27 +0700

    first commit

---
+++ all_function.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Sir.</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@1,400&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Prompt', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Array Colors</h1>
+    <?php
+    function showmessage()
+    {
+        echo"<center>";
+        echo"<h2>ยินดีต้อนรับสู่เว็บไซต์</h2>";
+        echo"<h3>เพื่อประโยชน์ของสมาชิก</h3>";
+        echo"<h4>โปรดติดตามข่าวสารจากเว็บไซต์ ผ่านจากสื่อ ดังนี้</h4>";
+        echo"<h5>Email, SMS, Webboard</h5>";
+        echo"<hr>";
+        echo"</center>";
+    }
+    ?>
+</body>
+
+</html>
---
+++ testfunction.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Sir.</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@1,400&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Prompt', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Array Colors</h1>
+    <?php
+    include"all_function.php";
+    echo"<center>";
+    echo"นี้คือข้อความปกติ ที่อยู่ในไฟล์";
+    echo "<hr>";
+    echo "</center>";
+    showmessage();
+    ?>
+</body>
+
+</html>
